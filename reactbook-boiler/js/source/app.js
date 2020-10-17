23d62c81166669b8ef7f57c610978e54867a6d47
seitamoe <pinkdoll.2co33@gmail.com>
2020-10-17 17:08:24 +0900

    app.jsの更新②

--- reactbook-boiler/js/source/app.js
+++ reactbook-boiler/js/source/app.js
@@ -1,3 +1,16 @@
+// ReactDOM.render(
+  // <h1>
+    // <Logo /> アプリケーションにようこそ！
+  // </h1>,
+  // document.getElementById("app")
+// );
+
+"use strict"; //必ず指定するようにする
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Logo from "./components/Logo";
+
 ReactDOM.render(
   <h1>
     <Logo /> アプリケーションにようこそ！
