d3ddc68b435ee38fc5f8f51d6d841a83c8284916
seitamoe <pinkdoll.2co33@gmail.com>
2020-10-17 17:01:48 +0900

    Logo.jsの更新②

--- reactbook-boiler/js/source/components/Logo.js
+++ reactbook-boiler/js/source/components/Logo.js
@@ -1,5 +1,15 @@
-var Logo = React.createClass({
-  render: function() {
+// 5.1.5.1 モジュール
+// var Logo = React.createClass({
+  // render: function() {
+    // return <div className = "Logo" />;
+  // }
+// });
+
+// 5.1.5.4 最終的なコード
+import React from "react";
+
+class Logo extends React.Component {
+  render() {
     return <div className = "Logo" />;
   }
-});
+}
