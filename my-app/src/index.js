ccc0dfa34255a2cf383e12f509052b5108e5c503
seitamoe <pinkdoll.2co33@gmail.com>
2020-11-21 16:34:04 +0900

    material①

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import './index.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
+
 
 // class Square extends React.Component {
 
@@ -175,11 +180,12 @@ class Game extends React.Component {
     this.state = {
       history: [
         {
-        squares: Array(9).fill(null),
-      }
-    ],
-    stepNumber: 0,
+          squares: Array(9).fill(null),
+        }
+      ],
+      stepNumber: 0,
       xIsNext: true,
+      isChecked: true,
     };
   }
 //   render() {
@@ -215,7 +221,11 @@ class Game extends React.Component {
       xIsNext: !this.state.xIsNext
     });
   }
-
+  handleChange(event) {
+    this.setState({
+      isChecked: !this.state.isChecked      
+  })
+  }
   jumpTo(step) {
     this.setState({
       stepNumber: step,
@@ -245,7 +255,14 @@ class Game extends React.Component {
     } else {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
-
+    // const useStyles = makeStyles((theme) => ({
+    //   root: {
+    //     '& > *': {
+    //       margin: theme.spacing(1),
+    //     },
+    //   },
+    // }));
+    // const classes = useStyles();
     return (
       <div className="game">
         <div className="game-board">
@@ -258,7 +275,65 @@ class Game extends React.Component {
           <div>{status}</div>
           <ol>{moves}</ol>
         </div>
+        <form>
+      <TextField
+        id="date"
+        label="Birthday"
+        type="date"
+        defaultValue="2017-05-24"
+        // className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+    </form>
+        <div>
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      </Button>
+      <div>
+      <Checkbox
+        checked={ this.state.isChecked }
+        onChange={this.handleChange}
+        inputProps={{ 'aria-label': 'primary checkbox' }}
+      />
+      <Checkbox
+        defaultChecked
+        color="primary"
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      />
+      <Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
+      <Checkbox disabled inputProps={{ 'aria-label': 'disabled checkbox' }} />
+      <Checkbox disabled checked inputProps={{ 'aria-label': 'disabled checked checkbox' }} />
+      <Checkbox
+        defaultChecked
+        indeterminate
+        inputProps={{ 'aria-label': 'indeterminate checkbox' }}
+      />
+      <Checkbox
+        defaultChecked
+        color="default"
+        inputProps={{ 'aria-label': 'checkbox with default color' }}
+      />
+      <Checkbox
+        defaultChecked
+        size="small"
+        inputProps={{ 'aria-label': 'checkbox with small size' }}
+      />
+    </div>
+    </div>
       </div>
+      
     );
   }
 }
