efdeb01dfb6d6a9ae93eb100c308c2636a396f82
seitamoe <pinkdoll.2co33@gmail.com>
2020-11-23 10:57:55 +0900

    Text Field

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -429,6 +429,53 @@ class Game extends React.Component {
       <FormHelperText>Be careful</FormHelperText>
     </FormControl>
     </div>
+    {/* Text Field */}
+    <form>
+      <div>
+        <TextField error id="standard-error" label="Error" defaultValue="Hello World" />
+        <TextField
+          error
+          id="standard-error-helper-text"
+          label="Error"
+          defaultValue="Hello World"
+          helperText="Incorrect entry."
+        />
+      </div>
+      <div>
+        <TextField
+          error
+          id="filled-error"
+          label="Error"
+          defaultValue="Hello World"
+          variant="filled"
+        />
+        <TextField
+          error
+          id="filled-error-helper-text"
+          label="Error"
+          defaultValue="Hello World"
+          helperText="Incorrect entry."
+          variant="filled"
+        />
+      </div>
+      <div>
+        <TextField
+          error
+          id="outlined-error"
+          label="Error"
+          defaultValue="Hello World"
+          variant="outlined"
+        />
+        <TextField
+          error
+          id="outlined-error-helper-text"
+          label="Error"
+          defaultValue="Hello World"
+          helperText="Incorrect entry."
+          variant="outlined"
+        />
+      </div>
+    </form>
     </div>
       </div>
       
