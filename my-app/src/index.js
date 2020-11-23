12154eb57be1ba9d1269fc3d32a35e74675e2f07
seitamoe <pinkdoll.2co33@gmail.com>
2020-11-23 11:25:12 +0900

    BottomNavigation

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -22,6 +22,12 @@ import VolumeUp from '@material-ui/icons/VolumeUp';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import Switch from '@material-ui/core/Switch';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import FolderIcon from '@material-ui/icons/Folder';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 // class Square extends React.Component {
 
@@ -476,6 +482,13 @@ class Game extends React.Component {
         />
       </div>
     </form>
+    {/* ボトムナビゲーション */}
+    <BottomNavigation>
+      <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
+      <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
+      <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
+      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+    </BottomNavigation>
     </div>
       </div>
       
