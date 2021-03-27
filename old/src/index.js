d98624be551f52286a3209c3f7c0e3c9849dc430
seitamoe <pinkdoll.2co33@gmail.com>
2021-03-27 11:21:10 +0900

    create old folder

---
+++ old/src/index.js
@@ -0,0 +1,547 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+import './index.css';
+// import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import Select from '@material-ui/core/Select';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+import VolumeDown from '@material-ui/icons/VolumeDown';
+import VolumeUp from '@material-ui/icons/VolumeUp';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Switch from '@material-ui/core/Switch';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import FolderIcon from '@material-ui/icons/Folder';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import clsx from 'clsx';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+// class Square extends React.Component {
+
+  // 状態を初期化するコンストラクタ－をクラスに追加
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     value: null,
+  //   }
+  // }
+  // render() {
+  //   return (
+      // <button className="square" onClick={function() { alert('click');}}>
+
+      // 矢印関数構文をしようする方法
+
+      // 初期設定
+      // <button className="square" onClick={() => alert('click')}>
+
+      // クリックされたら「X」マークになるように設定を変える
+      // <button className="square" onClick={() => this.setState({value: 'X'})}>
+  //     <button
+  //       className="square"
+  //       onClick={() => this.setState  ({value: 'X'})}>
+  //       {this.state.value}
+  //     </button>
+  //   );
+  // }
+
+  // render() {
+    function Square(props) {
+      return (
+        <button className="square" onClick={props.onClick}>
+          {props.value}
+        </button>
+      );
+    }
+  // }
+
+// }
+
+
+// class Board extends React.Component {
+  // ボードの初期状態を設定する
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     squares: Array(9).fill(null),
+  //     xIsNext: true,
+  //   };
+  // }
+  // 再び状態を持ち上げる
+  class Board extends React.Component {
+    // handleClick(i) {
+    //   const squares = this.state.squares.slice();
+    //   if (calculateWinner(squares) || squares[i]) {
+    //     return;
+    //   }
+    //   squares[i] = this.state.xIsNext ? 'X' : 'O';
+    //   this.setState({
+    //     squares: squares,
+    //     xIsNext: !this.state.xIsNext,
+    //   });
+    // }
+
+    renderSquare(i) {
+      return (
+        <Square
+          value={this.props.squares[i]}
+          onClick={() => this.props.onClick(i)}
+        />
+      );
+    }
+
+    render() {
+      // const winner = calculateWinner(this.state.squares);
+      // let status;
+      // if (winner) {
+      //   status = 'Winner:' + winner;
+      // } else {
+      //   status = 'Next player:' + (this.state.xIsNext ? 'X' : 'O');
+      // }
+
+      return (
+        <div>
+         // {/* <div className="status">{status}</div> */}
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+
+  // class Board extends React.Component {
+  //   renderSquare(i) {
+  //     return (
+  //       <Square
+  //       value={this.props.squares[i]}
+  //       onClick={() => this.props.onClick(i)}
+  //       />
+  //     );
+  //   }
+  // render() {
+  //   return (
+  //     <div>
+  //       <div className="board-row">
+  //         {this.renderSquare(0)}
+  //         {this.renderSquare(1)}
+  //         {this.renderSquare(2)}
+  //       </div>
+  //       <div className="board-row"> 
+  //         {this.renderSquare(3)}
+  //         {this.renderSquare(4)}
+  //         {this.renderSquare(5)}
+  //       </div>
+  //       <div className="board-row"> 
+  //         {this.renderSquare(6)}
+  //         {this.renderSquare(7)}
+  //         {this.renderSquare(8)}
+  //       </div>
+  //     </div>
+      
+  //   );
+  // }
+
+  // handleClick(i) {
+  //   const squares = this.state.squares.slice();
+  //   if (calculateWinner(squares) || squares[i]) {
+  //     return;
+  //   }
+    // squares[i] = 'X';
+
+    //交代での部分 「X」と「O」が交互に使用できるようになる
+  //   squares[i] = this.state.xIsNext ? 'X' : 'O'; 
+  //   this.setState({
+  //     squares: squares,
+  //     xIsNext: !this.state.xIsNext,
+  //   });
+  // }
+
+
+  // renderSquare(i) {
+    // return <Square value={i} />;
+  //   return (
+  //     <Square value={this.state.squares[i]}
+  //     onClick={() => this.handleClick(i)}
+  //      />
+  //   );
+  // }
+
+  
+// }
+
+class Game extends React.Component {
+  // 再び状態を持ち上げる
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true,
+      isChecked: true,
+    };
+  }
+//   render() {
+//     return (
+//       <div className="game">
+//         <div className="game-board">
+//           <Board />
+//         </div>
+//         <div className="game-info">
+//           <div>{/* status */}</div>
+//           <ol>{/* TODO */}</ol>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+  handleClick(i) {
+    const history = this.state.history.slice(0,this.state.stepNumber + 1);
+    const current = history[this.state.stepNumber];
+    // const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+  handleChange(event) {
+        this.setState({
+      isChecked: !this.state.isChecked      
+  })
+  }
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+    // const useStyles = makeStyles((theme) => ({
+    //   root: {
+    //     '& > *': {
+    //       margin: theme.spacing(1),
+    //     },
+    //   },
+    // }));
+    // const classes = useStyles();
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={i => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+        <form>
+      <TextField
+        id="date"
+        label="Birthday"
+        type="date"
+        defaultValue="2017-05-24"
+        // className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+    </form>
+        <div>
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      </Button>
+      <div>
+      <Checkbox
+        checked={ this.state.isChecked }
+        onChange={this.handleChange}
+        inputProps={{ 'aria-label': 'primary checkbox' }}
+      />
+      <Checkbox
+        defaultChecked
+        color="primary"
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      />
+      <Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
+      <Checkbox disabled inputProps={{ 'aria-label': 'disabled checkbox' }} />
+      <Checkbox disabled checked inputProps={{ 'aria-label': 'disabled checked checkbox' }} />
+      <Checkbox
+        defaultChecked
+        indeterminate
+        inputProps={{ 'aria-label': 'indeterminate checkbox' }}
+      />
+      <Checkbox
+        defaultChecked
+        color="default"
+        inputProps={{ 'aria-label': 'checkbox with default color' }}
+      />
+      <Checkbox
+        defaultChecked
+        size="small"
+        inputProps={{ 'aria-label': 'checkbox with small size' }}
+      />
+
+      {/* Radio */}
+          <FormControl component="fieldset">
+      <FormLabel component="legend">Gender</FormLabel>
+      <RadioGroup aria-label="gender" name="gender1" >
+        <FormControlLabel value="female" control={<Radio />} label="Female" />
+        <FormControlLabel value="male" control={<Radio />} label="Male" />
+        <FormControlLabel value="other" control={<Radio />} label="Other" />
+        <FormControlLabel value="disabled" disabled control={<Radio />} label="(Disabled option)" />
+      </RadioGroup>
+    </FormControl>
+    {/* select */}
+    <div>
+      <FormControl>
+        <InputLabel htmlFor="grouped-native-select">Grouping</InputLabel>
+        <Select native defaultValue="" id="grouped-native-select">
+          <option aria-label="None" value="" />
+          <optgroup label="Category 1">
+            <option value={1}>Option 1</option>
+            <option value={2}>Option 2</option>
+          </optgroup>
+          <optgroup label="Category 2">
+            <option value={3}>Option 3</option>
+            <option value={4}>Option 4</option>
+          </optgroup>
+        </Select>
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="grouped-select">Grouping</InputLabel>
+        <Select defaultValue="" id="grouped-select">
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <ListSubheader>Category 1</ListSubheader>
+          <MenuItem value={1}>Option 1</MenuItem>
+          <MenuItem value={2}>Option 2</MenuItem>
+          <ListSubheader>Category 2</ListSubheader>
+          <MenuItem value={3}>Option 3</MenuItem>
+          <MenuItem value={4}>Option 4</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+    {/* Slider */}
+    <div>
+      <Typography id="continuous-slider" gutterBottom>
+        Volume
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item>
+          <VolumeDown />
+        </Grid>
+        <Grid item xs>
+          <Slider aria-labelledby="continuous-slider" />
+        </Grid>
+        <Grid item>
+          <VolumeUp />
+        </Grid>
+      </Grid>
+      <Typography id="disabled-slider" gutterBottom>
+        Disabled slider
+      </Typography>
+      <Slider disabled defaultValue={30} aria-labelledby="disabled-slider" />
+    </div>
+    {/* Switch */}
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Assign responsibility</FormLabel>
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch  name="gilad" />}
+          label="Gilad Gray"
+        />
+        <FormControlLabel
+          control={<Switch name="jason" />}
+          label="Jason Killian"
+        />
+        <FormControlLabel
+          control={<Switch name="antoine" />}
+          label="Antoine Llorca"
+        />
+      </FormGroup>
+      <FormHelperText>Be careful</FormHelperText>
+    </FormControl>
+    </div>
+    {/* Text Field */}
+    <form>
+      <div>
+        <TextField error id="standard-error" label="Error" defaultValue="Hello World" />
+        <TextField
+          error
+          id="standard-error-helper-text"
+          label="Error"
+          defaultValue="Hello World"
+          helperText="Incorrect entry."
+        />
+      </div>
+      <div>
+        <TextField
+          error
+          id="filled-error"
+          label="Error"
+          defaultValue="Hello World"
+          variant="filled"
+        />
+        <TextField
+          error
+          id="filled-error-helper-text"
+          label="Error"
+          defaultValue="Hello World"
+          helperText="Incorrect entry."
+          variant="filled"
+        />
+      </div>
+      <div>
+        <TextField
+          error
+          id="outlined-error"
+          label="Error"
+          defaultValue="Hello World"
+          variant="outlined"
+        />
+        <TextField
+          error
+          id="outlined-error-helper-text"
+          label="Error"
+          defaultValue="Hello World"
+          helperText="Incorrect entry."
+          variant="outlined"
+        />
+      </div>
+    </form>
+    {/* ボトムナビゲーション */}
+    <BottomNavigation>
+      <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
+      <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
+      <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
+      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+    </BottomNavigation>
+    {/* Divider */}
+    <div>
+      {['left', 'right', 'top', 'bottom'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button>{anchor}</Button>
+          <Drawer anchor={anchor}>
+            
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+    </div>
+      </div>
+      
+    );
+  }
+}
+
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+// 勝者の宣言
+// 勝者とリターンをチェックする
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a,b,c] =lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] ) {
+      return squares[a];
+    }
+  }
+  return null;
+}
